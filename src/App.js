6fc61f37112b2ac1e70d21cf696074f31a0d6cc4
phrubel <phrubel42@gmail.com>
2021-03-15 14:14:22 +0600

    simple authentic with firebase

--- src/App.js
+++ src/App.js
@@ -1,23 +1,92 @@
-import logo from './logo.svg';
 import './App.css';
+import firebase from "firebase/app";
+import "firebase/auth";
+import firebaseConfig from './firebase.config';
+import { useState } from 'react';
 
+if (!firebase.apps.length) {
+  firebase.initializeApp(firebaseConfig);
+}
 function App() {
+  const googleProvider = new firebase.auth.GoogleAuthProvider();
+  const facebookProvider = new firebase.auth.FacebookAuthProvider();
+  const githubProvider = new firebase.auth.GithubAuthProvider();
+  const [user, setUser] = useState({})
+  const [fbUser, setFbUser] = useState({})
+
+  const handleGoogleSignIn = () => {
+    firebase.auth()
+      .signInWithPopup(googleProvider)
+      .then((result) => {
+        var user = result.user;
+        // console.log(user)
+        setUser(user)
+        // ...
+      }).catch((error) => {
+        // Handle Errors here.
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        // The email of the user's account used.
+        var email = error.email;
+        console.log(errorCode, errorMessage, email)
+        // ...
+      });
+  }
+
+  const handleFbSignIn = () => {
+    firebase
+      .auth()
+      .signInWithPopup(facebookProvider)
+      .then((result) => {
+        var credential = result.credential;
+        var user = result.user;
+        console.log(user)
+        setFbUser(fbUser)
+
+        // ...
+      })
+      .catch((error) => {
+        // Handle Errors here.
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        // The email of the user's account used.
+        var email = error.email;
+        console.log(errorMessage, errorCode, email)
+        // ...
+      });
+  }
+
+  const handleGithubSignIn = () => {
+    firebase
+      .auth()
+      .signInWithPopup(githubProvider)
+      .then((result) => {
+        var user = result.user;
+        setUser(user)
+        // ...
+      }).catch((error) => {
+        // Handle Errors here.
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        // The email of the user's account used.
+        var email = error.email;
+        console.log(errorCode, errorMessage, email)
+        // ...
+      });
+  }
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={handleGoogleSignIn}>Sign in With Google</button>
+      <h1>Name: {user.displayName}</h1>
+      <h2>Email: {user.email}</h2>
+      <img src={user.photoURL} alt="" />
+      <button onClick={handleFbSignIn}>Sign in With Facebook</button>
+      <h1>Name: {fbUser.displayName}</h1>
+      <h2>Email: {fbUser.email}</h2>
+      <img src={fbUser.photoURL} alt="" />
+      <button onClick={handleGithubSignIn}>Sign in With Github</button>
     </div>
   );
 }
---
+++ src/firebase.config.js
@@ -0,0 +1,11 @@
+
+const firebaseConfig = {
+    apiKey: "AIzaSyD3eRhfJkS20xD3BGAlsBGDt1wpOBHr8FU",
+    authDomain: "fire-auth2-c9c5a.firebaseapp.com",
+    projectId: "fire-auth2-c9c5a",
+    storageBucket: "fire-auth2-c9c5a.appspot.com",
+    messagingSenderId: "632989400198",
+    appId: "1:632989400198:web:53010c1619ac8c76b3a507"
+  };
+
+  export default firebaseConfig
